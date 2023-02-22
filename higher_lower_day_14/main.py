3aceb93dba74fc79eba98c21b918a35f8ab75796
KnQ4 <kcodewn@gmail.com>
2023-02-22 13:21:52 -0600

    Higher Lower Finished

--- higher_lower_day_14/main.py
+++ higher_lower_day_14/main.py
@@ -1,53 +1,92 @@
-import random
 from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
-def random_choices():
-    return random.choice(data)
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-def account_info(account):
-    name = account["name"]
-    description = account["description"]
-    country = account["country"]
-    return f"{name},a {description}, from {country}"
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
 
-def answer(chosen_a, chosen_b, guess):
-    if chosen_a > chosen_b:
-        return guess == "a"
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
     else:
-        return guess == "b"
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
 
-def play_game():
-    print("Welcome to higher or lower, please guess if A is higher or lower than B.")
-    score = 0
-    game_stop = False 
-    chosen_a = random_choices()
-    chosen_b = random_choices()
+game()
 
-    while not game_stop:
-        chosen_a = chosen_b 
-        chosen_b = random_choices()
+'''
 
-        while chosen_a == chosen_b:
-            chosen_b = random_choices()
+FAQ: Why does choice B always become choice A in every round, even when A had more followers? 
 
-        print(f"Does A: {account_info(chosen_a)}, have more followers?")
-        print("OR")
-        print(f"Does B: {account_info(chosen_b)}, have more followers?")
+Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
 
-        guess = input("Does 'A' or 'B' have more followers?: ").lower()
-        follower_a = chosen_a["follower_count"]
-        follower_b = chosen_b["follower_count"]
-        correct_answer = answer(follower_a, follower_b, guess)
+'''
 
-        if correct_answer:
-            score += 1
-            print(f"Correct! Your score is currently at {score}")
-        else:
-            print(f"You got it wrong. Your end score is {score}")
-            game_continue = input("Would you like to play again? 'y' or 'n': ")
-            if game_continue == 'y':
-                play_game()
-            else:
-                game_stop = True
-                print("Thank you for playing!")
-play_game()
+
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
